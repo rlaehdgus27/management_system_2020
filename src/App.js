6b1f89dacd4ba2b53dfc792856ad9af1226b1825
rlaehdgus27 <rlaehdgus27@gmail.com>
2020-11-17 12:49:30 +0900

    customer

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import React, { Component, component } from "react";
+import React, { Component } from "react";
 import Customer from "./component/Customer";
 import "./App.css";
 
-const customer = {
-  name: "홍길동",
-  birthday: "950427",
-  gender: "남자",
-  job: "지구인",
-};
+const customer = [
+  {
+    id: 1,
+    // image: "",
+    name: "서달미",
+    birthday: "950427",
+    gender: "여자",
+    job: "CEO",
+  },
+  {
+    id: 2,
+    // image: "",
+    name: "남도산",
+    birthday: "901107",
+    gender: "남자",
+    job: "개발자",
+  },
+  {
+    id: 3,
+    // image: "",
+    name: "한지평",
+    birthday: "880312",
+    gender: "남자",
+    job: "투자자",
+  },
+];
 
 class App extends Component {
   render() {
     return (
-      <Customer
-        name={customer.name}
-        birthday={customer.birthday}
-        gender={customer.gender}
-        job={customer.job}
-      />
+      <div>
+        {customer.map((c) => {
+          return (
+            <Customer
+              key={c.id}
+              id={c.id}
+              // image={c.image}
+              name={c.name}
+              birthday={c.birthday}
+              gender={c.gender}
+              job={c.job}
+            />
+          );
+        })}
+      </div>
     );
   }
 }
